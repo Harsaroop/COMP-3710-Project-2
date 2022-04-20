fe910548bc633443f20aeb1e3ad744186c8334bb
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2022-04-20 01:59:01 -0400

    update files

--- p1.py
+++ p1.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 #%matplotlib inline
 
-raw_data = pd.read_csv('Bias_correction_ucl.csv')
-x = raw_data[['station', 'Date', 'Present_Tmax', 'Present_Tmin', 'LDAPS_RHmin','LDAPS_RHmax', 'LDAPS_Tmax_lapse', 'LDAPS_Tmin_lapse', 'LDAPS_WS','LDAPS_LH', 'LDAPS_CC1', 'LDAPS_CC2', 'LDAPS_CC3', 'LDAPS_CC4','LDAPS_PPT1', 'LDAPS_PPT2', 'LDAPS_PPT3', 'LDAPS_PPT4', 'lat', 'lon','DEM', 'Slope', 'Next_Tmax', 'Next_Tmin']]
+raw_data = pd.read_csv('Bias_correction_ucl - Copy.csv')
+raw_data = raw_data.round(2)
+x = raw_data[['station', 'Present_Tmax', 'Present_Tmin', 'LDAPS_RHmin','LDAPS_RHmax', 'LDAPS_Tmax_lapse', 'LDAPS_Tmin_lapse', 'LDAPS_WS','LDAPS_LH', 'LDAPS_CC1', 'LDAPS_CC2', 'LDAPS_CC3', 'LDAPS_CC4','LDAPS_PPT1', 'LDAPS_PPT2', 'LDAPS_PPT3', 'LDAPS_PPT4', 'lat', 'lon','DEM', 'Slope', 'Next_Tmax', 'Next_Tmin']]
 y = raw_data['Solar radiation']
 
 from sklearn.model_selection import train_test_split
